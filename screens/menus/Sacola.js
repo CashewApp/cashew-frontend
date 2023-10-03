bd150ad35010851df84561e1917efa2e3809aa26
Erik Kato <zorkdosun@gmail.com>
2023-10-03 01:09:00 -0300

    novos arquivos

---
+++ screens/menus/Sacola.js
@@ -0,0 +1,9 @@
+import { Text, View, ImageBackground, TouchableOpacity, TextInput } from 'react-native';
+
+export default function Sacola() {
+    return (
+        <View className="flex bg-white h-full">
+            <Text className=" font-bold text-3xl w-full pl-5 mt-20 ">Minha Sacola</Text>
+        </View>
+    );
+}
